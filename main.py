5c6b6612fcfa98367c3c52db9b4a1cbba916d966
Hans Bélanger <hans.belanger@pm.me>
2020-11-23 11:19:27 -0500

    added more properties to the rolling window

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
 from timeline import Timelines
 
@@ -11,34 +12,33 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
 df = pd.read_csv('covid_impact_on_airport_traffic.csv')
-print(df)
+
 
 # Timelines.DisplayChile(df, 14)
 
 
 def fenetreFlottante(tailleFenetre, saut, df):
-    outputDf = pd.DataFrame(columns=['Date', 'MovingAverage'])
-    valueSum = 0
-    outOfBoundsValues = 0
+    tempDf = pd.DataFrame(columns=['Date', 'Value'])
+    outputDf = pd.DataFrame(columns=['Date', 'MovingAverage', 'Variance', 'StandardDeviation', 'Kurtosis', 'Skewness', 'Slope', 'StdError'])
 
     for j in range(0, int(len(df.index)), saut):
         for i in range(j, j + tailleFenetre):
-            if (i +1 > int(len(df.index))):
-                outOfBoundsValues += 1
-            else:
-                valueSum += df['PercentOfBaseline'].iloc[i]
-                print('j: ' + str(j) + ',  I: ' + str(i) + ', df value: ' + str(df['PercentOfBaseline'].iloc[i]))
+            if (i + 1 < int(len(df.index))):
+                tempRow = {'Date': df['Date'].iloc[i], 'Value': df['PercentOfBaseline'].iloc[i]}
+                tempDf = tempDf.append(tempRow, ignore_index=True)
 
-        print('valueSum: ' + str(valueSum))
-        print('out of bounds: ' + str(outOfBoundsValues))
-        print(valueSum / (tailleFenetre - outOfBoundsValues))
-        newRow = { 'Date':df['Date'].iloc[j] , 'MovingAverage':valueSum / (tailleFenetre - outOfBoundsValues)  }
-        outputDf = outputDf.append(newRow , ignore_index=True)
-        valueSum = 0
-        outOfBoundsValues = 0
-    outputDf= outputDf.set_index('Date')
+        slope, intercept, r_value, p_value, std_err = stats.linregress(tempDf.index, tempDf['Value'])
+        newRow = {'Date': df['Date'].iloc[j], 'MovingAverage': tempDf['Value'].mean(),
+                  'Variance': tempDf['Value'].var(), 'StandardDeviation': tempDf['Value'].std(),
+                  'Kurtosis': tempDf['Value'].kurtosis(), 'Skewness': tempDf['Value'].skew(), 'Slope':slope, 'StdError':tempDf['Value'].sem()}
+        outputDf = outputDf.append(newRow, ignore_index=True)
+        tempDf = tempDf.drop(tempDf.index, inplace=True)
+        tempDf = pd.DataFrame(columns=['Date', 'Value'])
 
+    outputDf = outputDf.set_index('Date')
+    print(outputDf)
     return (outputDf)
+
 
 # df_test['Btime'].iloc[0]
 
@@ -49,8 +49,13 @@ df.index = pd.DatetimeIndex(df['Date'])
 df = df.reindex(pd.date_range('2020-03-16', '2020-10-16'), fill_value=None)
 df = df.fillna(method='bfill')
 df = df.reset_index()
-#print( df)
+# print( df)
 newDf = fenetreFlottante(7, 4, df)
 
-newDf.plot()
-plt.show()
+
+
+'''df = pd.DataFrame(list, columns=['date', 'value'])
+df.date =pd.to_datetime(df.date)
+df['date_ordinal'] = pd.to_datetime(df['date']).map(dt.datetime.toordinal)
+slope, intercept, r_value, p_value, std_err = stats.linregress(df['date_ordinal'], df['value'])
+##'''
